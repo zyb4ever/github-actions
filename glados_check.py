b647dd4641f908b9c9f00c69699cbdef47181384
zyb4ever <zyb19930723@126.com>
2023-08-25 17:40:31 +0800

    Update glados_check.py

--- glados_check.py
+++ glados_check.py
@@ -12,7 +12,7 @@ headers = {
   'accept-language': 'zh-CN,zh;q=0.9',
   'authorization': '15530243933653734309100857125585-1080-1920',
   'content-type': 'application/json;charset=UTF-8',
-  'cookie': '_ga=GA1.1.2054971322.1681306585; koa:sess=eyJ1c2VySWQiOjM5MDMzNCwiX2V4cGlyZSI6MTcxODI2MzUyMjY2NiwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=j90B0LBvwpVfgorQ4B51rXwBV_8; _ga_CZFVKMNT9J=GS1.1.1692342374.2.1.1692344045.0.0.0',
+  'cookie': '_ga=GA1.1.2054971322.1681306585; koa:sess=eyJ1c2VySWQiOjM5MDMzNCwiX2V4cGlyZSI6MTcxODI2MzUyMjY2NiwiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=j90B0LBvwpVfgorQ4B51rXwBV_8; __stripe_mid=155e98c0-4823-465f-a85b-af375a17aea40afdc2; __stripe_sid=3cd7170a-c9cc-4564-b77a-b37b480e8ca13047fe; _ga_CZFVKMNT9J=GS1.1.1692954373.5.1.1692956137.0.0.0',
   'dnt': '1',
   'origin': 'https://glados.rocks',
   'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
